94b04ec610ec64693d5857a00900f3929930e431
Brad White <bradscottwhite@gmail.com>
2023-08-28 21:07:11 -0400

    connecting to server..

--- src/app.tsx
+++ src/app.tsx
@@ -63,18 +63,18 @@ export function App() {
 } catch (err) {
   console.log('errr', err)
 }*/
-(async () => {
-  await fetch('https://go-todo-api-production-4ec6.up.railway.app/todo/list', {
-    mode: 'no-cors',
-    headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiZXhwIjoxNjkzNDYzMjIxfQ.1poS8tAwQkWRsMuyg98M-vIJuFMyatfdddlCZb-67zc'
-    }
-  })
-    .then(res => console.log(JSON.stringify(res.json())))
-    .catch(err => console.log(err))
-  //.then(res => res.json());
-  //console.log(await updated);
-})();
+  const ping = async () => {
+    await fetch('https://go-todo-api-production-4ec6.up.railway.app/todo/list', {
+      //mode: 'no-cors',
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiZXhwIjoxNjkzNDY1NzI3fQ.K7gY5r588TXm0sZeuFGl2l6CMyyPKSdyx0F_KUjad9s'
+      }
+    })
+      .then(res => res.json())
+      .catch(err => console.log(err))
+    //.then(res => res.json());
+    //console.log(await updated);
+  };
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
@@ -85,6 +85,7 @@ export function App() {
       )*/}
 
       <h1 className='text-5xl text-red-800'>Howdy!</h1>
+      <button className='bg-red-600 rounded-2xl m-10' onClick={ping}>Ping</button>
     </main>
   )
 }
